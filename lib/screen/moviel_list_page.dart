40759b85fbbc948bd49b55a1d72ba3f50d18e500
Maan <mluqmaan22@gmail.com>
2026-03-09 17:02:22 +0700

    feat: halaman movie detail

--- lib/screen/moviel_list_page.dart
+++ lib/screen/moviel_list_page.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:latihan_kuis_a/models/movie_model.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 class MovieListPage extends StatelessWidget {
   final String nama;
@@ -11,9 +12,7 @@ class MovieListPage extends StatelessWidget {
       implementasikan widget untuk menampilkan list movie yang sudah diambil dari dummy data
         - gunakan ListView.builder untuk menampilkan list movie
         - setiap item movie ditampilkan menggunakan Card widget yang berisi judul movie dan poster movie
-        - ketika item movie ditekan, navigasikan ke halaman MovieDetailPage dengan membawa data movie yang dipilih
-        
-    
+        - ketika item movie ditekan, navigasikan ke halaman MovieDetailPage dengan membawa data movie yang dipilih 
      */
     return Scaffold(
       appBar: AppBar(
@@ -51,10 +50,77 @@ class MovieListPage extends StatelessWidget {
                 fit: BoxFit.cover,
               ),
               trailing: Icon(Icons.arrow_forward_ios, color: Colors.black),
-              onTap: () {},
+              onTap: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                    builder: (context) =>
+                        MovieDetailPage(movie: movieList[index]),
+                  ),
+                );
+              },
             ),
           );
         },
+      ),
+    );
+  }
+}
+
+class MovieDetailPage extends StatelessWidget {
+  final MovieModel movie;
+  const MovieDetailPage({super.key, required this.movie});
+
+  Future<void> _launchWikipedia() async {
+    final Uri uri = Uri.parse(movie.movieUrl);
+
+    if (!await launchUrl(uri, mode: LaunchMode.externalApplication)) {
+      print("Gagal buka link");
+    } else {
+      print("Tombol ke ${movie.movieUrl} berhasil diklik");
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(movie.title),
+        backgroundColor: Colors.blue,
+        foregroundColor: Colors.white,
+      ),
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            Image.network(movie.imgUrl),
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Column(
+                children: [
+                  Text(
+                    movie.title,
+                    style: TextStyle(fontWeight: FontWeight.bold, fontSize: 30),
+                  ),
+                  Text("${movie.year}"),
+                  Text("Genre: ${movie.genre}"),
+                  Text("Sutradara: ${movie.director}"),
+                  Text("Pemeran: ${movie.casts.join(", ")}"),
+                  Row(
+                    children: [
+                      Icon(Icons.star, color: Colors.amber),
+                      Text("${movie.rating}"),
+                    ],
+                  ),
+                  Text(movie.synopsis),
+                  ElevatedButton(
+                    onPressed: _launchWikipedia,
+                    child: Text("Ke Wikipedia"),
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
